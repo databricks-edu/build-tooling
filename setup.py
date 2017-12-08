973fff06e1ad4222884a91e5bfb051baedeae681
Brian Clapper <bmc@clapper.org>
2017-12-08 17:40:36 -0500

    Master install is an actual Python package

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ Can be run to install all the subpieces.
 '''
 
 from __future__ import print_function
+from setuptools import setup
 import os
 import sys
 from contextlib import contextmanager
+
+VERSION = '1.0.0'
 
 @contextmanager
 def chdir(dir):
@@ -23,13 +26,22 @@ def cmd(command):
 
 top_dir = os.path.dirname(os.path.abspath(__file__))
 
-with chdir(os.path.join(top_dir, 'gendbc')):
-    print('Installing gendbc...')
-    cmd('bin/activator install')
+if len(sys.argv) > 1 and sys.argv[1] == 'install':
+    with chdir(os.path.join(top_dir, 'gendbc')):
+        print('Installing gendbc...')
+        cmd('bin/activator install')
 
-for d in ('master_parse', 'bdc'):
-    print('Installing {0}...'.format(d))
-    with chdir(os.path.join(top_dir, d)):
-        cmd('python setup.py install')
+    for d in ('master_parse', 'bdc'):
+        print('Installing {0}...'.format(d))
+        with chdir(os.path.join(top_dir, d)):
+            cmd('python setup.py install')
 
-sys.exit(0)
+setup(
+    name='db-build-tooling',
+    package=[],
+    version=VERSION,
+    description='Wrapper package for Databricks Training build tools',
+    author='Databricks Education Team',
+    author_email='training-logins@databricks.com',
+    license="Creative Commons Attribution-NonCommercial 4.0 International",
+)

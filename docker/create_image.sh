8165f6b39921ab6f8fcb18a677c52ec51fef9d98
Brian Clapper <bmc@clapper.org>
2019-01-16 16:26:18 -0500

    Docker image is now based on stock Python 2 image. Added ability to test against a fork.

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -41,20 +41,20 @@ case $# in
         ;;
 esac
 
+: ${FORK:=databricks-edu}
+
 echo "Creating $DOCKERFILE"
 
-curl -o $DOCKERFILE https://raw.githubusercontent.com/docker-library/openjdk/master/8/jdk/Dockerfile
-
-sed -i.bak 's/FROM buildpack-deps:stretch-scm/FROM python:2/' $DOCKERFILE
-sed -i.bak 's/update.sh/create_image.sh/' $DOCKERFILE
-echo 'RUN pip install git+https://github.com/databricks-edu/build-tooling' >> $DOCKERFILE
+echo "FROM python:2" >$DOCKERFILE
+echo "RUN pip install git+https://github.com/$FORK/build-tooling" >> $DOCKERFILE
 echo 'RUN apt-get update && apt-get install -y less zip unzip' >> $DOCKERFILE
 echo 'RUN curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"' >> $DOCKERFILE
 echo 'RUN unzip awscli-bundle.zip' >> $DOCKERFILE
 echo 'RUN ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws' >> $DOCKERFILE
-echo 'RUN mv /root/local/bin/gendbc /usr/local/bin/' >> $DOCKERFILE
+#echo 'RUN mv /root/local/bin/gendbc /usr/local/bin/' >> $DOCKERFILE
 cp ../course $DOCKERFILEDIR
 echo 'ADD course /usr/local/bin' >> $DOCKERFILE
+
 
 USAGE=
 

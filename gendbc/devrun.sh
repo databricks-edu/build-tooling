77d48e35f3e90a5d6928edc099b58ebdf5deef70
Brian Clapper <bmc@clapper.org>
2019-02-07 16:10:05 -0800

    Updated for addition of new db_edu_util library.

--- gendbc/devrun.sh
+++ gendbc/devrun.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
 # Use this script to run the uninstalled (repo) version of "gendbc",
-# pulling "notebooktools" from the repo.
+# pulling "db_edu_util" from the repo.
 # -----------------------------------------------------------------------------
 
 here=`pwd`
 up=`(cd ..; pwd)`
 
-PYTHONPATH=.:$up/notebooktools python gendbc/__init__.py "$@"
+PYTHONPATH=.:$up/db_edu_util python gendbc/__init__.py "$@"

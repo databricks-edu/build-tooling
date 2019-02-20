fa9b7ab374c4d83e11ce423a9c2f57811c80f81a
Brian Clapper <bmc@clapper.org>
2019-02-20 12:38:34 -0500

    Added ability to use a different Docker snapshot.

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -5,10 +5,13 @@
 #
 #    . /path/to/build-tooling/docker/aliases.sh
 
-alias bdc='docker run -it --rm -w `pwd` -e DB_SHARD_HOME=$DB_SHARD_HOME -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool bdc'
-alias databricks='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool databricks'
-alias gendbc='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool gendbc'
-alias master_parse='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool master_parse'
+# Set BUILD_TOOL_DOCKER_TAG to a different tag (e.g., "snapshot") to use
+# a different tag.
+: ${BUILD_TOOL_DOCKER_TAG:=latest}
+alias bdc='docker run -it --rm -w `pwd` -e DB_SHARD_HOME=$DB_SHARD_HOME -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool:$BUILD_TOOL_DOCKER_TAG bdc'
+alias databricks='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool:$BUILD_TOOL_DOCKER_TAG databricks'
+alias gendbc='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool:$BUILD_TOOL_DOCKER_TAG gendbc'
+alias master_parse='docker run -it --rm -w `pwd` -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool:$BUILD_TOOL_DOCKER_TAG master_parse'
 
 COURSE_ENV_VARS="DB_CONFIG_PATH DB_PROFILE DB_SHARD_HOME COURSE_NAME COURSE_REPO COURSE_HOME COURSE_YAML COURSE_MODULES COURSE_REMOTE_SOURCE COURSE_REMOTE_TARGET PREFIX SOURCE TARGET EDITOR PAGER LESS LESSCHARSET USER"
 
@@ -27,7 +30,7 @@ function course {
 
   create_course_envfile $TMP_ENV
 
-  docker run -it --rm -w `pwd` --env-file $TMP_ENV -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool course "$@"
+  docker run -it --rm -w `pwd` --env-file $TMP_ENV -e HOME=$HOME -v $HOME:$HOME databrickseducation/build-tool:$BUILD_TOOL_DOCKER_TAG course "$@"
 
   rm -f $TMP_ENV
 }

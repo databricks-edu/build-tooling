5d499c9645f00c55cb234532de6b4c34ee0fe656
Brian Clapper <bmc@clapper.org>
2019-03-04 12:37:26 -0500

    Added unalias.sh, allowing easy undefining of the Docker aliases, for development testing.

---
+++ docker/unalias.sh
@@ -0,0 +1,11 @@
+# "." or "source" this file to unset the aliases and undefine
+# the functions defined in aliases.sh.
+#
+# Useful for development.
+
+unalias bdc
+unalias databricks
+unalias gendbc
+unalias master_parse
+unset -f course
+

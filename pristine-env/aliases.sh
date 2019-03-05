83a5ffb5fe03b4b4c4429cf81fa5130b42fd5608
Brian Clapper <bmc@clapper.org>
2019-03-05 18:23:06 -0500

    Got rid of debugging stuff

--- pristine-env/aliases.sh
+++ pristine-env/aliases.sh
@@ -8,10 +8,9 @@ function py3dock {
     shell=${SHELL:-bash}
   fi
   shell=$(basename $shell)
-  echo $shell
 
-  #docker run -it --rm -w `pwd` -e HOME=$HOME -e USER=$USER -e TERM=vt100 \
-  #  -v $HOME:$HOME pristine-python3 $shell
+  docker run -it --rm -w `pwd` -e HOME=$HOME -e USER=$USER -e TERM=vt100 \
+    -v $HOME:$HOME pristine-python3 $shell
 }
 
 alias pydock3=py3dock

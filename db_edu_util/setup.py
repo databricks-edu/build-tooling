30b162de1dfc2169e0243297d2d675cdc0a6a01b
Brian Clapper <bmc@clapper.org>
2019-03-06 21:51:28 -0500

    Added "python setup.py test" command

--- db_edu_util/setup.py
+++ db_edu_util/setup.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from distutils.cmd import Command
 import re
 import sys
+import os
 
 # Can't just import notebooktools, because dependencies have to be satisfied
 # first. So, we'll "grep" for the VERSION.
@@ -23,6 +25,37 @@ if not version:
     sys.stderr.write("Can't find version in {0}\n".format(source))
     sys.exit(1)
 
+def run_cmd(command_string):
+    import subprocess
+    try:
+        print(f'+ {command_string}')
+        rc = subprocess.call(command_string, shell=True)
+        if rc < 0:
+            print(f'Command terminated by signal {-rc}',
+                  file=sys.stderr)
+    except OSError as e:
+        print(f'Command failed: {e}', file=sys.stderr)
+
+class Test(Command):
+    description = 'run the Nose tests'
+
+    user_options = []
+
+    def __init__(self, dist):
+        Command.__init__(self, dist)
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Convention: Run the module to run the tests.
+        for module_path in ('db_cli.py',):
+            p = os.path.join('db_edu_util', module_path)
+            run_cmd('python ' + p)
+
 setup(
     name='db_edu_util',
     packages=['db_edu_util'],
@@ -30,6 +63,9 @@ setup(
     description='Library of stuff used by the build tools',
     install_requires=[
     ],
+    cmdclass = {
+        'test': Test
+    },
     author='Databricks Education Team',
     author_email='training-logins@databricks.com',
     classifiers=[],
